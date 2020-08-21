51fd396cc1d4054106f0ccf9c046d1408c4d9249
Marthin Silalahi <marthin.silalahi@anacle.com>
2020-08-21 15:58:29 +0800

    INITIALIZE PROJECT & GIT IGNORE

---
+++ FixFontSize/FixFontSize.Android/Renders/MyLabelRender.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Android.Content;
+using Android.Graphics;
+using FixFontSize.Droid.Renders;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(Label), typeof(MyLabelRender))]
+namespace FixFontSize.Droid.Renders
+{
+    public class MyLabelRender : LabelRenderer
+    {
+        public MyLabelRender(Context context) : base(context)
+        {
+
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Label> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.NewElement == null)
+                return;
+
+            if (Control != null)
+            {
+                // This will adjust from tag font size
+                //Control.SetTextSize(Android.Util.ComplexUnitType.Dip, (float)e.NewElement.FontSize);
+
+                // This is fix size
+                Control.SetTextSize(Android.Util.ComplexUnitType.Dip, (float)12.0);
+
+                // This will set font family & font attribute
+                Control.SetTypeface(Typeface.SansSerif, TypefaceStyle.Bold);
+            }
+        }
+    }
+}
